4eaa38b4d28ce819555cbbb25550765457d28949
HP <HP@DESKTOP-RJ52POG>
2024-07-28 01:46:21 +0530

    1

---
+++ jenkin1/src/main/java/jenkin/App.java
@@ -0,0 +1,19 @@
+package jenkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+        String s1="ganesh";
+        String s2 ="ganesh";
+        
+        System.out.println(s1 == s2);
+    }
+}
